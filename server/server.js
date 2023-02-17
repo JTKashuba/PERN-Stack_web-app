a4968db42f95605b34e052bbea7c37605790688e
JT-K <kashuba.jt@gmail.com>
2023-02-17 14:26:31 -0800

    configured logic for back-end API routes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@
 require('dotenv').config()
 const express = require("express");
 const db = require('./db')
-
 const morgan = require('morgan');
 
 const app = express();
@@ -11,20 +10,43 @@ const app = express();
 // JSON middleware
 app.use(express.json())
 
+
 // CRUD Operations ///////////////////////////////////////////////
+
+/* IMPORTANT LESSON
+
+    The following template string syntax is bad practice
+
+const results = await db.query(`SELECT * FROM restaurants where id = ${req.params.id}`);
+
+    It leaves the application vulnerable to SQL injection attacks; hence, 
+    it's never recommended to use any kind of string interpolation or template literals within a query.
+
+    Instead, use a parameterized query with the following syntax
+
+const results = await db.query("SELECT * FROM restaurants WHERE id = $1", [req.params.id]);
+*/
+
 /*
 Create
 */
 
 // Create Restaurant
-app.post("/api/v1/restaurants", (req, res) => {
-    console.log(req.body);
-    res.status(201).json({
-        status: "success",
-        data: {
-            restaurant: "mcdonalds"
-        }
-    });
+app.post("/api/v1/restaurants", async (req, res) => {
+    //console.log(req.body);
+    try {
+        const results = await db.query("INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) RETURNING *", [req.body.name, req.body.location, req.body.price_range]);
+        //console.log(results);
+        //console.log(results.rows[0]);
+        res.status(201).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0]
+            }
+        });
+    } catch(err) {
+        console.log(err);
+    }
 });
 
 /*
@@ -33,25 +55,36 @@ Read
 
 // Get All Restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
-    const results = await db.query("SELECT * FROM restaurants")
-    console.log(results);
-    res.status(200).json({
-        status: "success",
-        data: {
-            restaurant: ["mcdonalds", "wendys"]   
-        }
-    });
+    try {
+        const results = await db.query("SELECT * FROM restaurants");
+        //console.log(results);
+        res.status(200).json({
+            status: "success",
+            results: results.rows.length,
+            data: {
+                restaurants: results.rows
+            }
+        });
+    } catch(err) {
+        console.log(err);
+    }
 });
 
 // Get One Restaurant
-app.get("/api/v1/restaurants/:id", (req, res) => {
-    console.log(req.params);
-    res.status(200).json({
-        status: "success",
-        data: {
-            restaurant: "mcdonalds"
-        }
-    });
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+    //console.log(req.params.id);
+    try {
+        const results = await db.query("SELECT * FROM restaurants WHERE id = $1", [req.params.id]);
+        //console.log(results);
+        res.status(200).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0]
+            }
+        });
+    } catch(err) {
+        console.log(err);
+    }
 });
 
 /* 
@@ -59,15 +92,21 @@ Update
 */
 
 // Update Restaurant
-app.put("/api/v1/restaurants/:id", (req, res) => {
-    console.log(req.params.id);
-    console.log(req.body);
-    res.status(200).json({
-        status: "success",
-        data: {
-            restaurant: "mcdonalds"
-        }
-    });
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+    //console.log(req.params.id);
+    //console.log(req.body);
+    try {
+        const results = await db.query("UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *", [req.body.name, req.body.location, req.body.price_range, req.params.id]);
+        console.log(results);
+        res.status(200).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0]
+            }
+        });
+    } catch(err) {
+        console.log(err);
+    }
 });
 
 
@@ -76,11 +115,16 @@ Delete
 */
 
 // Delete Restaurant
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-    res.status(204).json({
-        status: "success"
-    });
-})
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+    try {
+        const results = await db.query("DELETE FROM restaurants WHERE id = $1", [req.params.id]);
+        res.status(204).json({
+            status: "success"
+        });
+    } catch(err) {
+        console.log(err);
+    }
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
