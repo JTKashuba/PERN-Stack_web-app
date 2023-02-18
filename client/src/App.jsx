f39240fa99a40ae5b2d874ac594eae2f02db2708
JT-K <kashuba.jt@gmail.com>
2023-02-17 17:35:55 -0800

    skeleton front-end REACT application, including Bootstrap (and font-awesome from cdnjs.com to use later when configuring star rating for Reviews)

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+/*
+    Visual Studio Code extension "ES7+ React/Redux/React-Native snippets" commands:
+        imr + [Tab] => import React from 'react'
+*/
+
+import React from 'react'
+
+const App = () => {
+    return <div>App</div>
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+/*
+    Visual Studio Code extension "ES7+ React/Redux/React-Native snippets" commands:
+        imr + [Tab] => import React from 'react'
+        imrd + [Tab] => import ReactDOM from 'react-dom'
+*/
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from "./App"
+
+ReactDOM.render(<App/>, document.getElementById("root"))
