d3c891773ac70796205e194ac44cfb9f557bd2e3
JT-K <kashuba.jt@gmail.com>
2023-03-02 17:31:11 -0800

    configured AddRestaurant component functionality on front-end with useState hooks to fetch user input. implemented logic to send post method to back-end: AddRestaurant component now updates postgres table from front-end user input and updates the data displayed on index page UI without reloading the page

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import RestaurantFinder from '../apis/RestaurantFinder';
+import { RestaurantsContext } from '../context/RestaurantsContext';
 
 const AddRestaurant = () => {
+    const {addRestaurants} = useContext(RestaurantsContext);
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [priceRange, setPriceRange] = useState("Price Range");
+
+    const handleSubmit = async (e) => {
+        {/* default behavior in HTML is to reload page. with React we never want to reload the page so we don't lose State */}
+        e.preventDefault()
+        try {
+            const response = await RestaurantFinder.post("/", {
+                name: name,
+                location: location,
+                price_range: priceRange
+            });
+            addRestaurants(response.data.data.restaurant);
+            console.log(response);
+        } catch (err) {
+            console.log(err);
+        }
+    }
   return (
     <div className="mb-4">
         <form action="">
             <div className="form-row">
                 <div className="col">
-                    <input type="text" className="form-control" placeholder="name"/>
+                    <input value={name} 
+                    onChange={(e) => setName(e.target.value)} 
+                    type="text" 
+                    className="form-control" 
+                    placeholder="name"/>
                 </div>
                 <div className="col">
-                    <input type="text" className="form-control" placeholder="location"/>
+                    <input value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                    type="text" 
+                    className="form-control" 
+                    placeholder="location"/>
                 </div>
                 <div className="col">
-                    <select className="custom-select my-1 mr-sm-2">
+                    <select value={priceRange}
+                    onChange={(e) => setPriceRange(e.target.value)}
+                    className="custom-select my-1 mr-sm-2">
                         <option disabled>Price Range</option>
                         <option value="1">$</option>
                         <option value="2">$$</option>
@@ -21,7 +53,7 @@ const AddRestaurant = () => {
                         <option value="5">$$$$$</option>
                     </select>
                 </div>
-                <button className="btn btn-primary">Add</button>
+                <button onClick={handleSubmit} type="submit"className="btn btn-primary">Add</button>
             </div>
         </form>
     </div>
