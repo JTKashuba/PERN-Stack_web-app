77fb4213145cff200dbcaeba6a3cf98f4f204661
JT-K <kashuba.jt@gmail.com>
2023-03-02 16:51:38 -0800

    retrieved data from postgres table on back-end server and rendered it on front-end UI index page

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -29,7 +29,18 @@ const RestaurantList = (props) => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
+                {restaurants && restaurants.map((restaurant) => {
+                    return (
+                    <tr key={restaurant.id}>
+                        <td>{restaurant.name}</td>
+                        <td>{restaurant.location}</td>
+                        <td>{"$".repeat(restaurant.price_range)}</td>
+                        <td>reviews</td>
+                        <td><button className="btn btn-warning">Update</button></td>
+                        <td><button className="btn btn-danger">Delete</button></td>
+                    </tr>);                    
+                })}
+                {/*<tr>
                     <td>tasty thai</td>
                     <td>eugene</td>
                     <td>$$</td>
@@ -44,7 +55,7 @@ const RestaurantList = (props) => {
                     <td>Rating</td>
                     <td><button className="btn btn-warning">Update</button></td>
                     <td><button className="btn btn-danger">Delete</button></td>
-                </tr>
+                </tr> */}
             </tbody>
         </table>
     </div>
