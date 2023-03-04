3a81e311f0c1bd5ad1f5a13e6e44b8bc03e7bb7f
JT-K <kashuba.jt@gmail.com>
2023-03-04 15:57:13 -0800

    made 'controlled forms' for Update Restaurant component (to control user input) by defining state with useState hooks, made page auto-populate name/loc/price fields for user convenience by fetching data for the restaurant of a given id

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,25 +1,55 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import RestaurantFinder from '../apis/RestaurantFinder';
 
 const UpdateRestaurant = (props) => {
-    {/* React Router hook to grab params from the URL */}
     const {id} = useParams();
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [priceRange, setPriceRange] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await RestaurantFinder.get(`/${id}`);
+            console.log(response.data.data);
+            setName(response.data.data.restaurant.name);
+            setLocation(response.data.data.restaurant.location);
+            setPriceRange(response.data.data.restaurant.price_range);
+        }
+        fetchData();
+    }, []);
+
     return (
         <div>
             <form action="">
                 <div className="form-group">
                     <label htmlFor="name">Name</label>
-                    <input id="name" className="form-control" type="text" />
+                    <input 
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        id="name"
+                        className="form-control"
+                        type="text" />
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="location">Location</label>
-                    <input id="location" className="form-control" type="text" />
+                    <input 
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                        id="location"
+                        className="form-control"
+                        type="text" />
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="price_range">Price Range</label>
-                    <input id="price_range" className="form-control" type="number" />
+                    <input 
+                        value={priceRange}
+                        onChange={(e) => setPriceRange(e.target.value)}
+                        id="price_range"
+                        className="form-control"
+                        type="number" />
                 </div>
                 <button className="btn btn-primary">Submit</button>
             </form>
