09c88e42225dfd66bd87242a9cff49da1f911faf
JT-K <kashuba.jt@gmail.com>
2023-03-04 15:36:17 -0800

    configured skeleton front-end for UpdateRestaurant component

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,11 +1,29 @@
-{/* SHORTCUT REMINDER: rafce + [Tab] in Visual Studio Code */}
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
-import React from 'react'
+const UpdateRestaurant = (props) => {
+    {/* React Router hook to grab params from the URL */}
+    const {id} = useParams();
+    return (
+        <div>
+            <form action="">
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input id="name" className="form-control" type="text" />
+                </div>
 
-const UpdateRestaurant = () => {
-  return (
-    <div>UpdateRestaurant</div>
-  )
+                <div className="form-group">
+                    <label htmlFor="location">Location</label>
+                    <input id="location" className="form-control" type="text" />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="price_range">Price Range</label>
+                    <input id="price_range" className="form-control" type="number" />
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>);
 }
 
 export default UpdateRestaurant
